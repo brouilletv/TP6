2d031c197d258c40382a1aebc6345bd753b461f0
Brouilletv <Brouilletv@educ.colleg-montreal.qc.ca>
2025-02-05 10:45:32 -0500

    v2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from game_state import GameState
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Modèle de départ"
+SCREEN_TITLE = "TP6"
 
 
 class MyGame(arcade.Window):
@@ -16,9 +16,22 @@ class MyGame(arcade.Window):
         arcade.set_background_color(arcade.color.GRAY)
 
         self.state = None
+
         self.rpc = None
         self.appuyer = None
+        self.score1 = None
+        self.score2 = None
+
         self.player = None
+        self.compy = None
+        self.static_list = None
+
+        self.square_list = None
+
+        self.rock = None
+        self.paper = None
+        self.cissors = None
+        self.move_list = None
 
         """
         self.player_attack_type = {
@@ -31,23 +44,48 @@ class MyGame(arcade.Window):
     def setup(self):
         self.state = GameState.NOT_STARTED
 
-        self.player = arcade.Sprite("assets/assets/faceBeard.png", 1, 100, 100)
+        self.rpc = arcade.Text("Roche, Papier, ciseaux", 100, 550, arcade.color.RED_DEVIL, 50)
+        self.appuyer = arcade.Text(" ", 100, 500, arcade.color.LIGHT_BLUE, 20)
+        self.score1 = arcade.Text("pointage du joueur", 100, 500, arcade.color.LIGHT_BLUE, 20)
+        self.score2 = arcade.Text("pointage de l'ordinateur", 100, 500, arcade.color.LIGHT_BLUE, 20)
+
+        self.player = arcade.Sprite("assets/assets/faceBeard.png", 0.4, 200, 300)
+        self.compy = arcade.Sprite("assets/assets/compy.png", 2, 600, 300)
+        self.static_list = arcade.SpriteList()
+        self.static_list.append(self.player)
+        self.static_list.append(self.compy)
+
+        self.square_list = [[30, 130, 100, 200], [160, 260, 100, 200], [290, 390, 100, 200], [550, 650, 100, 200]]
+
+        self.rock = arcade.Sprite("assets/assets/srock.png", 0.6, 80, 150)
+        self.paper = arcade.Sprite("assets/assets/spaper.png", 0.6, 210, 150)
+        self.cissors = arcade.Sprite("assets/assets/scissors.png", 0.6, 340, 150)
+        self.move_list = arcade.SpriteList()
+        self.move_list.append(self.rock)
+        self.move_list.append(self.paper)
+        self.move_list.append(self.cissors)
 
     def on_draw(self):
         self.clear()
-        arcade.set_background_color(arcade.color.GRAY)
 
-        arcade.draw_text("Roche, Papier, ciseaux", 100, 550, arcade.color.RED_DEVIL, 50)
-        self.player.draw()
+        self.rpc.draw()
+        self.static_list.draw()
+        self.score1.draw()
+        self.score2.draw()
+
+        for i in self.square_list:
+            arcade.draw_lrbt_rectangle_outline(i[0], i[1], i[2], i[3], arcade.color.REDWOOD)
 
         if self.state == GameState.NOT_STARTED:
-            arcade.draw_text("Appuyer sur 'Espace' pour commencer un nouveau round!", 100, 500, arcade.color.LIGHT_BLUE, 20)
+            self.appuyer.text = "Appuyer sur 'Espace' pour commencer un nouveau round!"
         elif self.state == GameState.ROUND_ACTIVE:
-            arcade.draw_text("Appuyer sur une image pour faire une attack!", 200, 500, arcade.color.LIGHT_BLUE, 20)
+            self.appuyer.text = "Appuyer sur une image pour faire une attack!"
+            self.move_list.draw()
         elif self.state == GameState.ROUND_DONE:
-            arcade.draw_text("Appuyer sur 'Espace' pour commencer un nouveau round!", 100, 500, arcade.color.LIGHT_BLUE, 20)
+            self.appuyer.text = "Appuyer sur 'Espace' pour commencer un nouveau round!"
         else:
             pass
+        self.appuyer.draw()
 
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.SPACE and self.state == GameState.NOT_STARTED:
